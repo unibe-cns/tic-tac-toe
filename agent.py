11aaa87e1746ab26d54f8e7e7683e75a47a5d573
Jakob Jordan <jakobjordan@posteo.de>
2022-07-23 15:47:14 +0200

    Add primitive training

--- agent.py
+++ agent.py
@@ -8,6 +8,8 @@ class Agent:
         row * 3 + col: (row, col) for row in range(3) for col in range(3)
     }
 
+    move_to_action_idx = {value: key for key, value in action_idx_to_move.items()}
+
     def __init__(self, seed):
         self.policy = {}
         self.rng = np.random.default_rng(seed)
@@ -16,6 +18,7 @@ class Agent:
         hsh = game.state_hash()
 
         if hsh not in self.policy:
+            print("new state", hsh)
             self.policy[hsh] = np.ones(9) * 1 / 9.0
 
         while True:
@@ -23,6 +26,13 @@ class Agent:
             move = self.action_idx_to_move[action_idx]
             if game.is_empty(move[0], move[1]):
                 return move
+
+    def load_policy(self, fn):
+        with open(fn, "r") as f:
+            policy = json.load(f)
+        for hsh in policy:
+            policy[hsh] = np.array(policy[hsh])
+        self.policy = policy
 
     def sample_action(self, probs):
         return self.rng.multinomial(1, pvals=probs)
--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import numpy as np
+
 from agent import Agent
 from game import Game
+
+
+def learn(agent, history, win):
+    if win:
+        d = 1.1
+    else:
+        d = 0.9
+
+    for hsh, move in history:
+        agent.policy[hsh][agent.move_to_action_idx[move]] *= d
+        agent.policy[hsh] /= np.sum(agent.policy[hsh])
+        assert np.all(agent.policy[hsh] >= 0.0)
+        assert np.abs(np.sum(agent.policy[hsh]) - 1.0) < 1e-9
 
 
 def main():
@@ -7,7 +22,12 @@ def main():
 
     seed = 1234
     agent = Agent(seed)
+    try:
+        agent.load_policy("./policy.json")
+    except FileNotFoundError:
+        pass
 
+    history = []
     while True:
         row, col = input("place marker: ").split(",")
         row, col = int(row), int(col)
@@ -16,17 +36,24 @@ def main():
         state = game.check_state()
         if state != Game.GameState.RUNNING:
             print(state)
+            if state != Game.GameState.DRAW:
+                learn(agent, history, False)
             break
 
-        row, col = agent.get_move(game)
+        move = agent.get_move(game)
+        history.append((game.state_hash(), move))
+        row, col = move
         game.mark(row, col, game.FieldState.CIRCLE)
+        print(history)
         print(game)
         state = game.check_state()
         if state != Game.GameState.RUNNING:
             print(state)
+            if state != Game.GameState.DRAW:
+                learn(agent, history, True)
             break
 
-    agent.save_policy("policy.json")
+    agent.save_policy("./policy.json")
 
 
 main()
