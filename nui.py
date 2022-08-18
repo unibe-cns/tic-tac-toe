d27638d4f09e790adeed3663757c626f068ab790
Jakob Jordan <jakobjordan@posteo.de>
2022-08-18 18:18:51 +0200

    Fix NUI

--- nui.py
+++ nui.py
@@ -7,7 +7,16 @@ class NUI:
     def listen_input(marker):
         assert False
 
+    def show_final_state(self, board, state, winner, winning_fields):
+        pass
+
+    def show_new_game(self):
+        pass
+
     def show_policy(self, values):
+        pass
+
+    def show_scores(self, scores):
         pass
 
     @staticmethod
